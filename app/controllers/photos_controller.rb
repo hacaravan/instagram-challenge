a2e423741904c51c27576537f1e55ba32177af1e
Hugh Cavanagh <hacavanagh@gmail.com>
2021-02-28 18:25:24 +0000

    Adding photos as resource in routes file, adding CRUD methods in photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,37 @@
 class PhotosController < ApplicationController
   def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.create(photo_params)
+    redirect_to photos_feed_url
   end
 
   def feed
+    @photos = Photo.all
   end
+
+  def update
+    @photo = Post.find(params[:id])
+    @photo.update(post_params)
+    redirect_to photos_feed_url
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    redirect_to photos_feed_url
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo, :user_id).permit(:caption)
+  end
+
 end
